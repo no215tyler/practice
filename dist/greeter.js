01525e9448f145961102e3a5b9a007613cf23c13
no215tyler <no.215.tyler@gmail.com>
2024-07-02 00:46:55 +0900

    TypeScript練習

---
+++ dist/greeter.js
@@ -0,0 +1,14 @@
+"use strict";
+class Student {
+    constructor(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+let user = new Student("Nobuhiko", "no.215", "Taira");
+document.body.textContent = greeter(user);
