5e6d1fb70c15a4f585721fd2a6e3e2553ffb08c9
no215tyler <no.215.tyler@gmail.com>
2024-07-07 23:09:47 +0900

    JavaScriptの標準入力値の受け取り

---
+++ test.js
@@ -0,0 +1,32 @@
+// ##################
+// 標準入力の受け取り方
+// ##################
+// readlineモジュールをインポート
+const readline = require('readline');
+
+// readlineインターフェースを作成
+const rl = readline.createInterface({
+  input: process.stdin, // 標準入力（通常はキーボード入力）を設定
+  output: process.stdout // 標準出力（通常はコンソール出力）を設定
+});
+
+// ユーザーに入力を促し、入力が行われるとコールバック関数が実行される
+rl.question('Enter a number: ', (answer) => {
+  // ユーザーの入力（文字列）を整数に変換
+  const n = parseInt(answer);
+  // 変換された整数をコンソールに出力
+  console.log(n);
+  // rl.close(); // readlineインターフェースを閉じる
+});
+
+// ##############################
+// 標準入力の別解 （onメソッドの使用）
+// ##############################
+rl.on('line', (input) => {
+  let str = input;
+  console.log(str);
+  // rl.close(); // readlineインターフェースを閉じる
+});
+
+
+
