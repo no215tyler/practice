2f5e69fd8050beca7efbdd50895fe38ce66fbcb6
no215tyler <no.215.tyler@gmail.com>
2024-07-08 23:43:25 +0900

    標準入力取得練習

--- JavaScript/自己紹介.js
+++ JavaScript/自己紹介.js
@@ -1,1 +1,16 @@
-console.log("name: Taira Nobuhiko\nlanguage: Ruby, JavaScript, Japanese\nhitokoto: Hello!")
+console.log("name: Taira Nobuhiko\nlanguage: Ruby, JavaScript, Japanese\n一言: Hello!")
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+  temp = input.split(' ');
+  temp.forEach(names => {
+    console.log(names);
+  });
+  rl.close();
+});
+
