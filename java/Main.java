08d728c6761ff1cb3878516575f608d253eb0016
no215tyler <no.215.tyler@gmail.com>
2024-09-02 12:12:11 +0900

    ️自己紹介.java

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "nobuhiko taira";
+    String language = "Ruby, JavaScript and learning Java";
+    String hitokoto = "Hello, Java World!";
+    String output = String.format("name: %s \nlanguage: %s \nhitokoto: %s", name, language, hitokoto);
+    System.out.println(output);
+  }
+}
