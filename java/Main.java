6eab6035ed94a737c8f42b779d90effd7305f63b
no215tyler <no.215.tyler@gmail.com>
2024-09-02 18:31:13 +0900

    GPT_Java問題集02: メソッドの定義

--- java/Main.java
+++ java/Main.java
@@ -1,17 +1,11 @@
-// 演習問題
-// 問題 1: 基本的な変数の定義
-// 年齢、名前、学生かどうかを表す変数を定義し、それらを出力するJavaプログラムを作成してください。
+// 問題 2: メソッドの定義
+// 引数として渡された数値を2倍にして返すメソッドdoubleValueを作成し、そのメソッドを使って値を2倍にして出力してください。
 class Main {
   public static void main(String[] args) {
-    int age = 23;
-    String name = "Jhon";
-    boolean isStudent = false;
-    if (isStudent) {
-      String output = String.format("%d歳の%sです。学生です。", age, name);
-      System.out.println(output);
-    } else {
-      String output = String.format("%d歳の%sです。学生ではありません。", age, name);
-      System.out.println(output);
-    }
+    System.out.println(doubleValue(10));
+  }
+
+  public static Integer doubleValue(int num) {
+    return num * 2;
   }
 }
