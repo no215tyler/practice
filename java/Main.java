ba9e6b568e79289e2490b44b06daf1df0de20b77
no215tyler <no.215.tyler@gmail.com>
2024-09-02 22:55:53 +0900

    カンマ区切りの3つのデータの入力 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,9 +2,8 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = Integer.parseInt(sc.next());
-    for (int i = 0; i < n; i++) {
-      String str = sc.next();
+    String[] strArray = sc.nextLine().split(",");
+    for (String str : strArray) {
       System.out.println(str);
     }
   }
