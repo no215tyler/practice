93d553f990ab4894cf05d7ffb1d994bc99039c6d
no215tyler <no.215.tyler@gmail.com>
2024-09-02 21:07:05 +0900

    1つのデータの入力 Java編

--- java/Main.java
+++ java/Main.java
@@ -1,16 +1,9 @@
-// 問題 3: 条件分岐
-// 引数として渡された数値が偶数か奇数かを判定し、偶数なら「Even」、奇数なら「Odd」と出力するメソッドcheckEvenOddを作成してください。
+import java.util.*;
 class Main {
   public static void main(String[] args) {
-    System.out.println(checkEvenOdd(64));
-    System.out.println(checkEvenOdd(303));
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    System.out.println(str);
   }
 
-  public static String checkEvenOdd(int num) {
-    if (num % 2 == 0) {
-      return "Even";
-    } else {
-      return "Odd";
-    }
-  }
 }
