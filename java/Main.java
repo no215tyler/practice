388d788b1b92b8713a7d40e421b2c102214bf3f5
no215tyler <no.215.tyler@gmail.com>
2024-09-02 22:28:36 +0900

    N行のデータの入力 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,8 +2,11 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String str = sc.nextLine();
-    System.out.println(str);
+    int num = Integer.parseInt(sc.nextLine());
+    for (int i = 0; i < num; i++) {
+      String str = sc.nextLine();
+      System.out.println(str);
+    }
   }
 
 }
