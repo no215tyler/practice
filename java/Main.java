e19a55793a5adba0df59e62adac0a8b841ee0f98
no215tyler <no.215.tyler@gmail.com>
2024-09-02 23:51:40 +0900

    GPT_Java問題集04: クラスの定義とオブジェクト生成

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,24 @@
-import java.util.*;
-class Main {
+// 問題 4: クラスの定義とオブジェクト生成
+// Carクラスを定義し、maker（メーカー）とmodel（モデル）の属性を持たせ、それらを表示するdisplayInfoメソッドを作成してください。
+// さらに、このクラスからオブジェクトを生成して、displayInfoメソッドを呼び出してみてください。
+
+public class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = Integer.parseInt(sc.nextLine());
-    String[] strArray = sc.nextLine().split(",");
-    for (String str : strArray) {
-      System.out.println(str);
-    }
+    Car car = new Car("TOYOTA", "ROOMY");
+    car.displayInfo();
+  }
+}
+
+class Car {
+  String maker;
+  String model;
+
+  public Car(String maker, String model) {
+    this.maker = maker;
+    this.model = model;
+  }
+
+  public void displayInfo(){
+    System.out.println(String.format("メーカー: %s\nモデル: %s", maker, model));
   }
 }
