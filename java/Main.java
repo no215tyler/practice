bd589e332b4b5b30945b7dc6e7101e9ce6d947c1
no215tyler <no.215.tyler@gmail.com>
2024-09-03 14:08:46 +0900

    引き算・掛け算 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,10 +2,13 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int N = 0;
-    N += 3286;
-    N *= 4736;
-    N %= 12312;
-    System.out.println(N);
+    String[] strNums = sc.nextLine().split(" ");
+    int[] nums = new int[strNums.length];
+    for (int i = 0; i < strNums.length; i++) {
+      nums[i] = Integer.parseInt(strNums[i]);
+    }
+    int D = nums[0] - nums[1];
+    int P = nums[0] * nums[1];
+    System.out.println(String.format("%d %d", D, P));
   }
 }
