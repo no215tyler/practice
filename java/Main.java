8d551bc7ac1484656fcf635009b7affb96c4748d
no215tyler <no.215.tyler@gmail.com>
2024-09-03 20:26:24 +0900

    半角スペースとバーティカルライン区切りで 10 個出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,9 +3,11 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String[] strNums = new String[10];
-    strNums = sc.nextLine().split(" ");
-    String result = String.join(",", strNums);
-    System.out.println(String.format("%s,", result));
+    for (int i = 0; i < 10; i++) {
+      strNums[i] = sc.next();
+    }
+    String result = String.join(" | ", strNums);
+    System.out.println(result);
     sc.close();
   }
 }
