021612574308abd4abd99bce599cd02ce973a474
no215tyler <no.215.tyler@gmail.com>
2024-09-03 17:00:19 +0900

    数値の分岐 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,16 +2,11 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String str = sc.nextLine();
-    if (Objects.equals(str, "paiza")) {
+    int num = sc.nextInt();
+    if (num <= 100) {
       System.out.println("YES");
     } else {
       System.out.println("NO");
     }
   }
 }
-
-// 文字列比較は「==演算子」ではなくequalsメソッドを使う
-// 文字列変数.equals(比較対象);
-// もしくは
-// Objects.equals(文字列, 比較対象);
