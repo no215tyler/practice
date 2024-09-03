d26c4cf3cf504ca431dd6ac65e9258fcbd8260dc
no215tyler <no.215.tyler@gmail.com>
2024-09-03 23:40:20 +0900

    【n * n の 2 次元配列の表示】N * N の九九表の出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -1,8 +1,11 @@
+import java.util.*;
 public class Main {
   public static void main(String[] args) {
-    for (int i = 1; i <= 9; i++) {
-      for (int j = 1; j <= 9; j++) {
-        if (j == 9) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= N; j++) {
+        if (j == N) {
           System.out.print(i * j);
         } else {
           System.out.print((i * j) + " ");
