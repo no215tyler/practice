0784a9d8ba36145bae5244f97e6e517fa395d2f4
no215tyler <no.215.tyler@gmail.com>
2024-09-03 17:17:32 +0900

    FizzBuzz Java編

--- java/Main.java
+++ java/Main.java
@@ -2,9 +2,16 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt();
-    for (int i = 1; i <= num; i++) {
-      System.out.println(i);
+    for (int i = 1; i <= 100; i++) {
+      if (i % 5 == 0 && i % 3 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      } else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      } else {
+        System.out.println(i);
+      }
     }
 
     sc.close();
