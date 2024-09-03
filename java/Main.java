c1e56ba4dd1f8c938a6d6829ab5337a5005b5c53
no215tyler <no.215.tyler@gmail.com>
2024-09-03 17:13:58 +0900

    ある数字までの出力 2 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,18 +2,11 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String[] strNums = sc.nextLine().split(" ");
-    int[] nums = new int[strNums.length];
-    for (int i = 0; i < strNums.length; i++) {
-      nums[i] = Integer.parseInt(strNums[i]);
+    int num = sc.nextInt();
+    for (int i = 1; i <= num; i++) {
+      System.out.println(i);
     }
-    int A = nums[0];
-    int B = nums[1];
-    int C = nums[2];
-    if ((A * B) <= C) {
-      System.out.println("YES");
-    } else {
-      System.out.println("NO");
-    }
+
+    sc.close();
   }
 }
