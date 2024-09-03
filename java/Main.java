e07dd934cf5044596859ba97e4727b1ab851721c
no215tyler <no.215.tyler@gmail.com>
2024-09-04 00:09:59 +0900

    GPT_Java問題集08: インターフェースの実装

--- java/Main.java
+++ java/Main.java
@@ -1,37 +1,19 @@
-// 問題 7: 継承の活用
-// Personクラスを作成し、名前と年齢を持つようにします。
-// このクラスを親クラスとしてStudentクラスを作成し、学年を追加してください。
-// また、introduceメソッドをオーバーライドし、"私は◯◯です。◯年生です。"と表示するようにしてください。
-class Parson {
-  String name;
-  int age;
-
-  public Parson(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  public void introduce() {
-    System.out.println(String.format("私は%sです。%d歳です。", name, age));
-  }
+// 問題 8: インターフェースの実装
+// Drawableというインターフェースを定義し、drawというメソッドを含めます。
+// そして、このインターフェースを実装するCircleクラスを作成し、drawメソッドで円を描くメッセージを表示してください。
+interface Drawable {
+  void draw();
 }
 
-class Student extends Parson {
-  int grade;
-
-  public Student(String name, int age, int grade) {
-    super(name, age);
-    this.grade = grade;
-  }
-  @Override
-  public void introduce() {
-    System.out.println(String.format("私は%sです。%d年生です。", name, grade));
+class Circle {
+  public void draw() {
+    System.out.println("円を描きます");
   }
 }
 
 public class Main {
   public static void main(String[] args) {
-    Student student = new Student("piko", 13, 1);
-    student.introduce();
+    Circle circle = new Circle();
+    circle.draw();
   }
 }
