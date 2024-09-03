9c7e2b07a563e3492a3d0b69e3168a235fa23ed5
no215tyler <no.215.tyler@gmail.com>
2024-09-03 20:22:35 +0900

    カンマ区切りで 10 個出力 1 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String[] strs = sc.nextLine().split(" ");
-    for (String str : strs) {
-      System.out.println(str);
-    }
+    String[] strNums = new String[10];
+    strNums = sc.nextLine().split(" ");
+    String result = String.join(",", strNums);
+    System.out.println(String.format("%s,", result));
     sc.close();
   }
 }
