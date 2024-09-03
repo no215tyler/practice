1e292dbbd09d54d6211fc5c7175a9d6971357b94
no215tyler <no.215.tyler@gmail.com>
2024-09-03 13:42:59 +0900

    累乗 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int A = 437326;
-    int B = 9085;
-    int X = A / B;
-    int Y = A % B;
-    System.out.println(String.format("%d %d", X, Y));
+    int A = 202;
+    int B = 134;
+    int C = 107;
+    int N = (int) Math.pow(((A + B) * C), 2);
+    System.out.println(N);
   }
 }
