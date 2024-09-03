639b0555e093b48423823a06f0aaf8528436dd96
no215tyler <no.215.tyler@gmail.com>
2024-09-03 19:39:39 +0900

    入力された 10 個の文字列を出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String[] strNums = new String[1000];
-    for (int i = 0; i < 1000; i++) {
-        strNums[i] = "" + (i + 1);
+    String[] strs = new String[10];
+    for (int i = 0; i < 10; i++) {
+      strs[i] = sc.next();
     }
-    String result = String.join(" ", strNums);
+    String result = String.join(" ", strs);
     System.out.println(result);
     sc.close();
   }
