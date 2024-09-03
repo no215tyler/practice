d03abe18683cf013c7d7715d0f4908e65e1029f9
no215tyler <no.215.tyler@gmail.com>
2024-09-04 00:02:24 +0900

    GPT_Java問題集07: 継承の活用

--- java/Main.java
+++ java/Main.java
@@ -1,17 +1,37 @@
-import java.util.*;
+// 問題 7: 継承の活用
+// Personクラスを作成し、名前と年齢を持つようにします。
+// このクラスを親クラスとしてStudentクラスを作成し、学年を追加してください。
+// また、introduceメソッドをオーバーライドし、"私は◯◯です。◯年生です。"と表示するようにしてください。
+class Parson {
+  String name;
+  int age;
+
+  public Parson(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public void introduce() {
+    System.out.println(String.format("私は%sです。%d歳です。", name, age));
+  }
+}
+
+class Student extends Parson {
+  int grade;
+
+  public Student(String name, int age, int grade) {
+    super(name, age);
+    this.grade = grade;
+  }
+  @Override
+  public void introduce() {
+    System.out.println(String.format("私は%sです。%d年生です。", name, grade));
+  }
+}
+
 public class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    for (int i = 1; i <= N; i++) {
-      for (int j = 1; j <= N; j++) {
-        if (j == N) {
-          System.out.print(i * j);
-        } else {
-          System.out.print((i * j) + " ");
-        }
-      }
-      System.out.println();
-    }
+    Student student = new Student("piko", 13, 1);
+    student.introduce();
   }
 }
