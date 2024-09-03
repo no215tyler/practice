2ae06e65d50523a58969fd162e3b86253e6c880e
no215tyler <no.215.tyler@gmail.com>
2024-09-03 17:05:34 +0900

    数値演算結果で分岐 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,8 +2,15 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt();
-    if (num <= 100) {
+    String[] strNums = sc.nextLine().split(" ");
+    int[] nums = new int[strNums.length];
+    for (int i = 0; i < strNums.length; i++) {
+      nums[i] = Integer.parseInt(strNums[i]);
+    }
+    int A = nums[0];
+    int B = nums[1];
+    int C = nums[2];
+    if ((A * B) <= C) {
       System.out.println("YES");
     } else {
       System.out.println("NO");
