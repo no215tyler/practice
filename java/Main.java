b6fd9a128feba9cd1a8f729ab2a80c86b5e66650
no215tyler <no.215.tyler@gmail.com>
2024-09-05 01:01:48 +0900

    1 つの実数を出力 4 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,8 +3,10 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    double num = sc.nextDouble();
-    System.out.println(String.format("%.3f", num));
+    String[] strNums = sc.nextLine().split(" ");
+    double floatNum = Double.parseDouble(strNums[0]);
+    int n = Integer.parseInt(strNums[1]);
+    System.out.println(String.format("%." + n + "f", floatNum));
     sc.close();
   }
 }
