d6dae92fdc1e5e89de21acbd8294fa93c30a1a4a
no215tyler <no.215.tyler@gmail.com>
2024-09-04 22:38:10 +0900

    【行によって長さが違う二次元配列の表示】すべての行の長さと値が不定な 2 次元配列の出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,23 +3,41 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int N = Integer.parseInt(sc.nextLine());
-    String[] strNums = sc.nextLine().split(" ");
-    int[] nums = new int[N];
-    for (int i = 0; i < N; i++) {
-      nums[i] = Integer.parseInt(strNums[i]);
-    }
+    // 入力を受け取る
+    String[] NUMS = sc.nextLine().split(" ");
+    String[] strNumsA = sc.nextLine().split(" ");
+    String[] strNumsB = sc.nextLine().split(" ");
 
-    for (int num : nums) {
-      for (int i = 1; i <= num; i++) {
-        if (i == num) {
-          System.out.println(i);
+    // 数値に分割
+    int N = Integer.parseInt(NUMS[0]);
+    int M = Integer.parseInt(NUMS[1]);
+
+    // 関数で数値型配列を戻す
+    ArrayList<Integer> numsA = compileToArrayNums(N, strNumsA);
+    ArrayList<Integer> numsB = compileToArrayNums(M, strNumsB);
+
+    for (int num : numsB) {
+      for (int i = 0; i < num; i++) {
+        System.out.print(numsA.get(0));
+        numsA.remove(0);
+        if (i == num - 1) {
+          System.out.println();
         } else {
-          System.out.print(i + " ");
+          System.out.print(" ");
         }
       }
     }
 
     sc.close();
+  }
+
+
+  public static ArrayList<Integer> compileToArrayNums(int N, String[] strNums) {
+    ArrayList list = new ArrayList<>();
+    for (int i = 0; i < N; i++) {
+      list.add(Integer.parseInt(strNums[i]));
+    }
+
+    return list;
   }
 }
