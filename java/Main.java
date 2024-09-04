e19fc10f2ce91fc4774c06ae8a35a4165139b6f4
no215tyler <no.215.tyler@gmail.com>
2024-09-05 01:04:33 +0900

    【実数をフォーマット指定して出力】複数の実数を出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String[] strNums = sc.nextLine().split(" ");
-    double floatNum = Double.parseDouble(strNums[0]);
-    int n = Integer.parseInt(strNums[1]);
-    System.out.println(String.format("%." + n + "f", floatNum));
+    int N = Integer.parseInt(sc.nextLine());
+    for (int i = 0; i < N; i++) {
+      String[] strNums = sc.nextLine().split(" ");
+      double floatNum = Double.parseDouble(strNums[0]);
+      int n = Integer.parseInt(strNums[1]);
+      System.out.println(String.format("%." + n + "f", floatNum));
+    }
     sc.close();
   }
 }
