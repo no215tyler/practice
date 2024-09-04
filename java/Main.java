d5ec441a262ae9b3833a63319ac184ccb52edb4c
no215tyler <no.215.tyler@gmail.com>
2024-09-04 17:57:02 +0900

    数値を異なる長さで 2 行出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -4,19 +4,22 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
-    int halfNum = N / 2;
-    for (int i = 1; i <= halfNum; i++) {
-      if (i == halfNum) {
-        System.out.println(i);
+    int M = sc.nextInt();
+    for (int i = 1; i <= N; i++) {
+      System.out.print(i);
+      if (i == N) {
+        System.out.println();
       } else {
-        System.out.print(i + " ");
+        System.out.print(" ");
       }
     }
-    for (int i = (halfNum + 1); i <= N; i++) {
-      if (i == N) {
-        System.out.println(i);
+
+    for (int i = 1; i <= M; i++) {
+      System.out.print(i);
+      if (i == M) {
+        System.out.println();
       } else {
-        System.out.print(i + " ");
+        System.out.print(" ");
       }
     }
 
