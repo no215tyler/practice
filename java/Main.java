bea55f42dc9395bed6a340cafe1450d855eba919
no215tyler <no.215.tyler@gmail.com>
2024-09-04 21:46:49 +0900

    すべての行の長さが不定な 2 次元配列の出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,17 +3,19 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    ArrayList<Integer> list = new ArrayList<>();
+    int N = Integer.parseInt(sc.nextLine());
+    String[] strNums = sc.nextLine().split(" ");
+    int[] nums = new int[N];
+    for (int i = 0; i < N; i++) {
+      nums[i] = Integer.parseInt(strNums[i]);
+    }
 
-    for (int i = 1; i <= N; i++) {
-      list.add(i);
-      for (int j = 0; j < list.size(); j++) {
-        System.out.print(list.get(j));
-        if (j == list.size() - 1) {
-          System.out.println();
+    for (int num : nums) {
+      for (int i = 1; i <= num; i++) {
+        if (i == num) {
+          System.out.println(i);
         } else {
-          System.out.print(" ");
+          System.out.print(i + " ");
         }
       }
     }
