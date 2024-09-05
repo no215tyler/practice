d12490360fd8ec382745c50da45a650486329c93
no215tyler <no.215.tyler@gmail.com>
2024-09-05 12:51:18 +0900

    数値を半角スペース埋めで出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -4,12 +4,8 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int N = Integer.parseInt(sc.nextLine());
-    for (int i = 0; i < N; i++) {
-      String[] strNums = sc.nextLine().split(" ");
-      double floatNum = Double.parseDouble(strNums[0]);
-      int n = Integer.parseInt(strNums[1]);
-      System.out.println(String.format("%." + n + "f", floatNum));
-    }
+    int num = Integer.parseInt(sc.nextLine());
+    System.out.println(String.format("%" + N + "d", num));
     sc.close();
   }
 }
