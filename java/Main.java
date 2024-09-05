cf8a43ad9f6842bf111aac9179f6b3856663ad8d
no215tyler <no.215.tyler@gmail.com>
2024-09-05 13:14:47 +0900

    【出力幅を指定して出力】N 個の数値を M けた半角スペース埋めで出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,11 +3,14 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int N = Integer.parseInt(sc.nextLine());
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+
     for (int i = 0; i < N; i++) {
-      int num = Integer.parseInt(sc.nextLine());
-      System.out.println(String.format("%3d", num));
+      int num = sc.nextInt();
+      System.out.println(String.format("%" + M + "d", num));
     }
+
     sc.close();
   }
 }
