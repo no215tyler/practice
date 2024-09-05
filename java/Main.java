cbb43db7d0117e6eaa2eb950bb9c500452622dd6
no215tyler <no.215.tyler@gmail.com>
2024-09-06 07:28:15 +0900

    【N 行 M 列のデータの入力】行ごとに要素数の異なる整数列の入力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,29 +3,17 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String[] strNums = sc.nextLine().split(" ");
-    int H = Integer.parseInt(strNums[0]);
-    int W = Integer.parseInt(strNums[1]);
-    int A = Integer.parseInt(strNums[2]);
-    int B = Integer.parseInt(strNums[3]);
-    int separatorCount = (W * 22) + ((W - 1) * 3);
-
-    for (int i = 0; i < H; i++) {
-      for (int j = 0; j < W; j++) {
-        System.out.print(String.format("(%9d, %9d)", A, B));
-        if (j == W - 1) {
-          System.out.println();
-          if (!(i == H - 1)) {
-            String[] separator = new String[separatorCount];
-            for (int k = 0; k < separatorCount; k++) {
-              separator[k] = "=";
+    int N = Integer.parseInt(sc.nextLine());
+    for (int i = 0; i < N; i++) {
+        String[] nums = sc.nextLine().split(" ");
+        for (int j = 1; j < nums.length; j++) {
+            System.out.print(nums[j]);
+            if (j == nums.length - 1) {
+              System.out.println();
+            } else {
+              System.out.print(" ");
             }
-            System.out.println(String.join("", separator));
-          }
-        } else {
-          System.out.print(" | ");
         }
-      }
     }
 
     sc.close();
