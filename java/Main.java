300d3487bd17a4ecacb0507c994a2d9d22e54f2f
no215tyler <no.215.tyler@gmail.com>
2024-09-05 12:59:21 +0900

    数値をゼロ埋めで出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,28 @@
 import java.util.*;
+import java.text.DecimalFormat;
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int N = Integer.parseInt(sc.nextLine());
+    DecimalFormat df = new DecimalFormat("000");
     int num = Integer.parseInt(sc.nextLine());
-    System.out.println(String.format("%" + N + "d", num));
+    System.out.println(df.format(num));
     sc.close();
   }
 }
+
+// 模範解答
+// import java.util.*;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+
+//         System.out.printf("%03d\n", n);
+
+//         sc.close();
+//     }
+// }
