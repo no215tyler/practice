3032f91f749dedcb0b0e3ea69327655e2cba40af
no215tyler <no.215.tyler@gmail.com>
2024-09-05 13:08:23 +0900

    複数の数値を半角スペース埋めで出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,8 +3,11 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int num = Integer.parseInt(sc.nextLine());
-    System.out.println(String.format("%03d", num));
+    int N = Integer.parseInt(sc.nextLine());
+    for (int i = 0; i < N; i++) {
+      int num = Integer.parseInt(sc.nextLine());
+      System.out.println(String.format("%3d", num));
+    }
     sc.close();
   }
 }
