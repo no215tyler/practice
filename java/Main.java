d8f75e08e56ed772c581feaa552529efa50edb79
no215tyler <no.215.tyler@gmail.com>
2024-09-05 13:26:03 +0900

    文字列と N 個の整数の出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,9 +3,20 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String S = sc.nextLine();
-    String T = sc.nextLine();
-    System.out.println(String.format("%s + %s = %s%s", S, T, S, T));
+    String[] strNums = sc.nextLine().split(" ");
+    int N = Integer.parseInt(strNums[0]);
+    String A = strNums[1];
+    String B = strNums[2];
+
+    for (int i = 0; i < N; i++) {
+      System.out.print(String.format("(%s, %S)", A, B));
+      if (i == N - 1) {
+        System.out.println();
+      } else {
+        System.out.print(", ");
+      }
+    }
+
     sc.close();
   }
 }
