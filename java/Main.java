bcd42f2eafefc840c5d4e7392870d24003aee35b
no215tyler <no.215.tyler@gmail.com>
2024-09-05 14:04:11 +0900

    ペアの数値の入った表を罫線入りで出力 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,13 +3,24 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    for (int i = 1; i <= 9; i++) {
-      for (int j = 1; j <= 9; j++) {
-        System.out.print(String.format("%2d", i * j));
-        if (j == 9) {
+    String[] strNums = sc.nextLine().split(" ");
+    int H = Integer.parseInt(strNums[0]);
+    int W = Integer.parseInt(strNums[1]);
+    String A = strNums[2];
+    String B = strNums[3];
+    int separatorCount = (W * 6) + ((W - 1) * 3);
+
+    for (int i = 0; i < H; i++) {
+      for (int j = 0; j < W; j++) {
+        System.out.print(String.format("(%s, %s)", A, B));
+        if (j == W - 1) {
           System.out.println();
-          if (!(i == 9)) {
-            System.out.println("==========================================");
+          if (!(i == H - 1)) {
+            String[] separator = new String[separatorCount];
+            for (int k = 0; k < separatorCount; k++) {
+              separator[k] = "=";
+            }
+            System.out.println(String.join("", separator));
           }
         } else {
           System.out.print(" | ");
