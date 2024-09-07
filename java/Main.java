ba4f607709664857d127a8de79ef0096d2c64f76
no215tyler <no.215.tyler@gmail.com>
2024-09-07 23:44:40 +0900

    文字列の配列 Java編

--- java/Main.java
+++ java/Main.java
@@ -3,62 +3,26 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    // 1行目の処理
-    String[] strNums = sc.nextLine().split(" ");
-    int N = Integer.parseInt(strNums[0]);
-    int Q = Integer.parseInt(strNums[1]);
 
-    // 2行目の処理
-    String[] bufNums = sc.nextLine().split(" ");
-    ArrayList<Integer> list = new ArrayList<>();
-    for (String num : bufNums) {
-      list.add(Integer.parseInt(num));
+    // 1行目の処理
+    String[] inputNums = sc.nextLine().split(" ");
+    int H = Integer.parseInt(inputNums[0]);
+    int W = Integer.parseInt(inputNums[1]);
+    int r = Integer.parseInt(inputNums[2]) - 1;
+    int c = Integer.parseInt(inputNums[3]) - 1;
+
+    String[][] board = new String[H][W];
+    for (int i = 0; i < H; i++) {
+      board[i] = sc.nextLine().split("");
     }
 
-    // 3行目以降の処理
-    for (int i = 0; i < Q; i++) {
-      int query = sc.nextInt();
-      switch (query) {
-        case 0:
-          int functionArgs = sc.nextInt();
-          pushBack(list, functionArgs);
-          break;
-
-        case 1:
-          popBack(list);
-          break;
-
-        case 2:
-          print(list);
-          break;
-      }
+    // 変数r, cで指定した位置が壁（#）かそうじゃないか判定
+    if (board[r][c].equals("#")) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
     }
 
     sc.close();
   }
-
-  // 要素の追加
-  public static ArrayList<Integer> pushBack(ArrayList<Integer> list, int n) {
-    list.add(n);
-    return list;
-  }
-
-  // 要素の削除
-  public static ArrayList<Integer> popBack(ArrayList<Integer> list) {
-    list.removeLast(); // == list.remove(list.size() - 1);
-    return list;
-  }
-
-  // 要素の出力
-  public static void print(ArrayList<Integer> list) {
-    for (int i = 0; i < list.size(); i++) {
-      System.out.print(list.get(i));
-      if (!(i == list.size() - 1)) {
-        System.out.print(" ");
-      } else {
-        System.out.println();
-      }
-    }
-  }
-
 }
