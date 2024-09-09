ca2493d6b6811a377284bac65e20b9b02254d1da
no215tyler <no.215.tyler@gmail.com>
2024-09-09 15:22:39 +0900

    代入演算 4 Java編

--- java/Main.java
+++ java/Main.java
@@ -4,15 +4,13 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    // 1行目の処理
-    String[] inputNums = sc.nextLine().split(" ");
-    int A = Integer.parseInt(inputNums[0]);
-    int B = Integer.parseInt(inputNums[1]);
-    int C = Integer.parseInt(inputNums[2]);
-    int D = Integer.parseInt(inputNums[3]);
+    String[] inputNum = sc.nextLine().split(" ");
+    int A = Integer.parseInt(inputNum[0]);
+    int B = Integer.parseInt(inputNum[1]);
 
-    int N = (int) Math.pow(((A + B) * C), 2) % D;
-
+    int N = 10000;
+    N /= A;
+    N %= B;
     System.out.println(N);
 
     sc.close();
