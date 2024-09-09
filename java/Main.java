d0640efca1da1e55d2b09aeb0e24da292d72c83f
no215tyler <no.215.tyler@gmail.com>
2024-09-09 16:02:27 +0900

    巨大な数のかけ算 Java編

--- java/Main.java
+++ java/Main.java
@@ -1,14 +1,15 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    String[] inputNum = sc.nextLine().split(" ");
-    int A = Integer.parseInt(inputNum[0]);
-    int B = Integer.parseInt(inputNum[1]);
+    BigInteger S = sc.nextBigInteger();
+    BigInteger T = sc.nextBigInteger();
+    BigInteger result = S.multiply(T);
 
-    System.out.println(A ^ B);
+    System.out.println(result);
 
     sc.close();
   }
