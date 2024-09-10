ae36746aa921e2dfdd3fc35dc8a35b288b932064
no215tyler <no.215.tyler@gmail.com>
2024-09-10 09:52:06 +0900

    【二次元配列の入出力】i番目の出力 Boss Java編

--- java/Main.java
+++ java/Main.java
@@ -6,16 +6,20 @@ public class Main {
 
     String[] inputStrNums = sc.nextLine().split(" ");
     int N = Integer.parseInt(inputStrNums[0]);
-    int K = Integer.parseInt(inputStrNums[1]) - 1;
+    int M = Integer.parseInt(inputStrNums[1]);
+    int K = Integer.parseInt(inputStrNums[2]) - 1;
+    int L = Integer.parseInt(inputStrNums[3]) - 1;
 
-    String[] strNumsArray = sc.nextLine().split(" ");
-    int[] numsArray = new int[N];
+    int[][] numsArray = new int[N][M];
 
     for (int i = 0; i < N; i++) {
-      numsArray[i] = Integer.parseInt(strNumsArray[i]);
+      String[] strNumsArray = sc.nextLine().split(" ");
+      for (int j = 0; j < M; j++) {
+        numsArray[i][j] = Integer.parseInt(strNumsArray[j]);
+      }
     }
 
-    System.out.println(numsArray[K]);
+    System.out.println(numsArray[K][L]);
 
     sc.close();
   }
