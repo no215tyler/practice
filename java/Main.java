4c896dd264b37f348ffeea360df31a9571eb51cc
no215tyler <no.215.tyler@gmail.com>
2024-09-11 19:44:49 +0900

    指定の要素のカウント Java編

--- java/Main.java
+++ java/Main.java
@@ -1,16 +1,22 @@
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] inputNums = sc.nextLine().split(" ");
+        int N = Integer.parseInt(inputNums[0]);
+        int K = Integer.parseInt(inputNums[1]);
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
 
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int[] nums = {0, 1};
-    for (int i = 0; i < N; i++) {
-      System.out.println(nums[0]);
-      int buf = nums[0] + nums[1];
-      nums[0] = nums[1];
-      nums[1] = buf;
+        int count = 0;
+        for (int i : nums) {
+            if (i == K) {
+                count += 1;
+            }
+        }
+        System.out.println(count);
     }
-  }
 }
