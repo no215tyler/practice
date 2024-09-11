81c17c2e5ca3f514b38cb128f555d413cc8e0b63
no215tyler <no.215.tyler@gmail.com>
2024-09-11 14:41:24 +0900

    重複の削除 Java編

--- java/Main.java
+++ java/Main.java
@@ -2,17 +2,51 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    int[] nums = {1, 3, 5, 1, 2, 3, 6, 6, 5, 1, 4};
 
-    String[] inputStrNums = sc.nextLine().split(" ");
-    int N = Integer.parseInt(inputStrNums[0]);
-    int K = Integer.parseInt(inputStrNums[1]) - 1;
+    ArrayList<Integer> result = new ArrayList<>();
 
-    String[] strs = sc.nextLine().split(" ");
-    Arrays.sort(strs);
+    for (int i = 1; i < nums.length; i++) {
+      checkUnique(result, nums[i]);
+    }
+    Collections.sort(result);
+    for (int num : result) {
+      System.out.println(num);
+    }
+  }
 
-    System.out.println(strs[K]);
+  public static ArrayList<Integer> checkUnique(ArrayList<Integer> result, int num) {
+    if (result.size() > 0) {
+      boolean flg = false;
 
-    sc.close();
+      // メイン処理
+      for (int i = 0; i < result.size(); i++) {
+        if (flg == false && result.get(i) == num) {
+          flg = true;
+          break;
+        } else if (flg == false && result.get(i) != num && i == (result.size() - 1)) {
+          result.add(num);
+        }
+      }
+    } else { // ArrayListの中身が空なら追加する
+      result.add(num);
+    }
+
+    return result;
   }
 }
+
+// 【模範回答】
+// import java.util.*;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         List<Integer> li = Arrays.asList(1, 3, 5, 1, 2, 3, 6, 6, 5, 1, 4);
+//         Set<Integer> a = new TreeSet(li);
+
+//         for (int x : a) {
+//             System.out.println(x);
+//         }
+//     }
+// }
