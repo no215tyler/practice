e0bc1494f362a0fd26eb9589b50a144c3a3f8761
no215tyler <no.215.tyler@gmail.com>
2024-09-12 23:01:31 +0900

    0 が含まれていないか判定 Java編

--- java/Main.java
+++ java/Main.java
@@ -4,18 +4,24 @@ public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      String[] inputNums = sc.nextLine().split(" ");
-      int N = Integer.parseInt(inputNums[0]);
-      int K = Integer.parseInt(inputNums[1]);
-
+      int N = sc.nextInt();
       int[] nums = new int[N];
 
       for (int i = 0; i < N; i++) {
         nums[i] = sc.nextInt();
       }
 
+      boolean flg = false;
       for (int num : nums) {
-        System.out.println(num + K);
+        if (num == 0) {
+          flg = true;
+        }
+      }
+
+      if (flg == true) {
+        System.out.println("NO");
+      } else {
+        System.out.println("YES");
       }
 
       sc.close();
