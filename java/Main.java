fbabf19000a3d09302c7d468a826e57de99402cd
no215tyler <no.215.tyler@gmail.com>
2024-09-13 15:50:14 +0900

    【Java Arrays.stream練習問題】1: 偶数の合計を求める

--- java/Main.java
+++ java/Main.java
@@ -1,19 +1,33 @@
+// 【Java Arrays.stream練習問題】
+// 問題 1: 偶数の合計を求める
+// 整数の配列が与えられます。配列の中から偶数を抽出し、その合計を求めてください。
+
+// 例
+// 入力: {1, 2, 3, 4, 5, 6}
+// 出力: 12 （2 + 4 + 6）
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
 
-      int[] nums = {-100, 2, 3, 5, 8};
+      String[] inputNums = sc.nextLine().split(" ");
+      int[] nums = new int[inputNums.length];
 
-      int min = Arrays.stream(nums)
-                      .min()
-                      .getAsInt();
+      for (int i = 0; i < inputNums.length; i++) {
+        nums[i] = Integer.parseInt(inputNums[i]);
+      }
 
-      int max = Arrays.stream(nums)
-                      .max()
-                      .getAsInt();
+      int result = Arrays.stream(nums).filter(n -> n % 2 == 0).sum();
+      System.out.println(result);
 
-      System.out.println(min);
-      System.out.println(max);
+      sc.close();
     }
 }
+
+// フィードバック
+// forループで文字列から整数に変換する方法としてStreamを使ってもっと簡潔に記述する方法
+// int[] nums = Arrays.stream(sc.nextLine().split(" "))
+//                    .mapToInt(Integer::parseInt)
+//                    .toArray();
