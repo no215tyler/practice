67d09c22e23d2bb69d1cbed51a211c436c801e84
no215tyler <no.215.tyler@gmail.com>
2024-09-13 09:28:10 +0900

    崖に落ちるか判定 Java編

--- java/Main.java
+++ java/Main.java
@@ -4,16 +4,17 @@ public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      int N = sc.nextInt();
+      String[] inputNums = sc.nextLine().split(" ");
+      int N = Integer.parseInt(inputNums[0]);
+      int K = Integer.parseInt(inputNums[1]);
+      int T = Integer.parseInt(inputNums[2]);
 
-      if ((N % 5 == 0) && (N % 3 == 0)) {
-        System.out.println("FizzBuzz");
-      } else if (N % 5 == 0) {
-        System.out.println("Buzz");
-      } else if (N % 3 == 0) {
-        System.out.println("Fizz");
+      int walkDistance = N * K;
+
+      if (walkDistance > T) {
+        System.out.println("NO");
       } else {
-        System.out.println(N);
+        System.out.println("YES");
       }
 
       sc.close();
