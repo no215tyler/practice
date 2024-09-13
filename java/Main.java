962140bf6952f66185afb2f5bcc6fe49f9d818c6
no215tyler <no.215.tyler@gmail.com>
2024-09-13 21:53:21 +0900

    【Java Arrays.stream練習問題】5: 文字列をアルファベット順にソートする

--- java/Main.java
+++ java/Main.java
@@ -1,9 +1,9 @@
-// 問題 4: 配列内の最小値と最大値を求める
-// 整数の配列が与えられます。その中の最小値と最大値をそれぞれ求めてください。
+// 問題 5: 文字列をアルファベット順にソートする
+// 文字列の配列が与えられます。その配列をアルファベット順にソートして出力してください。
 
 // 例
-// 入力: {5, 9, 2, 4, 7, 3}
-// 出力: 最小値: 2, 最大値: 9
+// 入力: {"Banana", "Apple", "Cherry", "Date"}
+// 出力: {"Apple", "Banana", "Cherry", "Date"}
 
 import java.util.*;
 
@@ -11,12 +11,41 @@ public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      int[] nums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-      int min = Arrays.stream(nums).min().getAsInt();
-      int max = Arrays.stream(nums).max().getAsInt();
-
-      System.out.println(String.format("最小値: %d 最大値: %d", min, max));
+      String[] strs = sc.nextLine().split(" ");
+      int[] fruitCount = {strs.length};
+      Arrays.stream(strs).sorted().forEach(fruit -> {
+        fruitCount[0] -= 1;
+        System.out.print(fruit);
+        if (fruitCount[0] == 0) {
+          System.out.println();
+        } else {
+          System.out.print(", ");
+        }
+      });
 
       sc.close();
     }
 }
+
+// フィードバック
+// 改善点:
+// 冗長な出力制御:
+
+// fruitCount[0]を使用してカウントダウンし、カンマを制御する方法は工夫されていますが、Collectors.joiningを使うとよりシンプルに実装可能です。以下のようにすることでコードの可読性が向上します。
+// java
+// コードをコピーする
+// System.out.println(Arrays.stream(strs).sorted().collect(Collectors.joining(", ")));
+// Collectors.joining(", ")を使うと、カンマ区切りで要素を簡単に出力できます。
+
+// 配列の長さ取得方法の冗長さ:
+
+// fruitCountで配列の長さを保持する必要はなく、strs.lengthで必要に応じてそのまま使えます。また、配列長を使ってカウントダウンするのはやや冗長です。
+// 変数名の適切さ:
+
+// fruitCountという変数名は、ソートと出力制御の文脈では適切ではないかもしれません。機能に即した名前を付けるとわかりやすくなります。
+// 【例】
+    // String[] strs = sc.nextLine().split(" ");
+    // // Collectors.joiningを使ってカンマ区切りの文字列を生成
+    // System.out.println(Arrays.stream(strs).sorted().collect(Collectors.joining(", ")));
+
+
