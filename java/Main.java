885a0ca676d182dc6f486a69fb8a5b2d2c0f6482
no215tyler <no.215.tyler@gmail.com>
2024-09-13 21:26:29 +0900

    【Java Arrays.stream練習問題】4: 配列内の最小値と最大値を求める

--- java/Main.java
+++ java/Main.java
@@ -1,9 +1,9 @@
-// 問題 3: 各要素を2倍にして新しい配列を作る
-// 整数の配列が与えられます。各要素を2倍にした新しい配列を作成してください。
+// 問題 4: 配列内の最小値と最大値を求める
+// 整数の配列が与えられます。その中の最小値と最大値をそれぞれ求めてください。
 
 // 例
-// 入力: {1, 2, 3, 4, 5}
-// 出力: {2, 4, 6, 8, 10}
+// 入力: {5, 9, 2, 4, 7, 3}
+// 出力: 最小値: 2, 最大値: 9
 
 import java.util.*;
 
@@ -12,29 +12,11 @@ public class Main {
       Scanner sc = new Scanner(System.in);
 
       int[] nums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-      int[] multiplyNums = Arrays.stream(nums).map(n -> n * 2).toArray();
+      int min = Arrays.stream(nums).min().getAsInt();
+      int max = Arrays.stream(nums).max().getAsInt();
 
-      for (int i = 0; i < multiplyNums.length; i++) {
-        System.out.print(multiplyNums[i]);
-        if (i == multiplyNums.length - 1) {
-          System.out.println();
-        } else {
-          System.out.print(", ");
-        }
-      }
+      System.out.println(String.format("最小値: %d 最大値: %d", min, max));
 
       sc.close();
     }
 }
-
-// 【フィードバック】
-// 改善点:
-// forループでの出力処理:
-// forループ内でカンマ区切りの処理を行っていますが、これはString.joinやCollectors.joiningといったメソッドを使うことでより簡潔に記述できます。
-// 例としてCollectors.joining(", ")を使うと、カンマ区切りの処理がループを使わずに済みます。
-
-// String result = Arrays.stream(nums)
-//                       .map(n -> n * 2)
-//                       .mapToObj(String::valueOf)
-//                       .collect(Collectors.joining(", "));
-
