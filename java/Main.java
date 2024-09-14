1c3ed696d6002d7d818491017e44a4183b7b6178
no215tyler <no.215.tyler@gmail.com>
2024-09-14 20:14:42 +0900

    未知数個の数の受け取り Java編

--- java/Main.java
+++ java/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      int N = Integer.parseInt(sc.nextLine());
+
       String input = sc.nextLine();
       if (input.isEmpty()) {
         System.out.println("配列が空です");
@@ -12,9 +12,13 @@ public class Main {
           int[] nums = Arrays.stream(input.split(" "))
                       .mapToInt(Integer::parseInt)
                       .toArray();
-          long count = Arrays.stream(nums).filter(n -> n == 1).count();
-          System.out.println(count);
 
+          for (int num : nums) {
+            System.out.println(num);
+            if (num == -1) {
+              break;
+            }
+          }
         } catch (NumberFormatException e) {
           System.out.println("不正な入力があります: " + e.getMessage());
         }
