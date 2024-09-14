d728a602e8e3cfcd0e1aed823bc13b15036c9308
no215tyler <no.215.tyler@gmail.com>
2024-09-14 17:25:58 +0900

    【Java Arrays.stream練習問題】15: 配列内の全要素を特定の数値で割った余りを計算して新しい配列を作成する

--- java/Main.java
+++ java/Main.java
@@ -1,18 +1,19 @@
-// 問題 14: 配列の合計値が特定の数値以上かどうかを確認する
-// 整数の配列と特定の数値が与えられます。配列内のすべての要素の合計が、特定の数値以上であるかどうかを確認してください。
+// 問題 15: 配列内の全要素を特定の数値で割った余りを計算して新しい配列を作成する
+// 整数の配列と特定の数値が与えられます。その配列内の全要素を特定の数値で割った余りを計算し、新しい配列を作成してください。
 
-// 例 入力: 配列: {10, 20, 30}, 数値: 50
-// 出力: true
+// 例 入力: 配列: {10, 20, 30}, 数値: 3
+// 出力: {1, 2, 0}
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       System.out.println("配列に代入する数値を半角スペース区切りで入力してください");
       String input = sc.nextLine();
-      System.out.println("しきい値を入力してください");
-      int threshold = Integer.parseInt(sc.nextLine());
+      System.out.println("割り算する数値を入力してください");
+      int N = Integer.parseInt(sc.nextLine());
 
       if (input.isEmpty()) {
         System.out.println("配列が空です");
@@ -22,23 +23,17 @@ public class Main {
                       .mapToInt(Integer::parseInt)
                       .toArray();
 
-          int sumResult = Arrays.stream(nums).sum();
-          if (sumResult >= threshold) {
-            System.out.println(true);
-          } else {
-            System.out.println(false);
-          }
+          var result = Arrays.stream(nums).map(n -> n % N).mapToObj(String::valueOf).collect(Collectors.joining(", "));
+          System.out.println(result);
+
 
         } catch (NumberFormatException e) {
           System.out.println("不正な入力があります: " + e.getMessage());
+        } catch (ArithmeticException e) {
+          System.out.println("0で割ることはできません / エラーメッセージ: " + e.getMessage());
         }
       }
 
       sc.close();
     }
 }
-
-// 【フィードバック】
-// 改善の余地:
-// Booleanの出力: 合計がしきい値以上の場合に true を表示し、それ以外は false を表示していますが、
-// より簡潔に System.out.println(sumResult >= threshold); とすることが可能です。
