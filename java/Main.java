90ffa6541bf5a79e72642e77ece57693b78407b9
no215tyler <no.215.tyler@gmail.com>
2024-09-15 02:21:12 +0900

    forループ Java編

--- java/Main.java
+++ java/Main.java
@@ -3,40 +3,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      String bufNum = sc.nextLine();
-
-      String inputA = sc.nextLine();
-      String inputB = sc.nextLine();
-      if (inputA.isEmpty() || inputB.isEmpty()) {
+      String input = sc.nextLine();
+      if (input.isEmpty()) {
         System.out.println("配列が空です");
       } else {
         try {
-          int[] numsA = Arrays.stream(inputA.split(" "))
-                      .mapToInt(Integer::parseInt)
-                      .toArray();
-          int[] numsB = Arrays.stream(inputB.split(" "))
+          int[] nums = Arrays.stream(input.split(" "))
                       .mapToInt(Integer::parseInt)
                       .toArray();
 
-          int A = Arrays.stream(numsA).max().getAsInt();
-          int AA = Arrays.stream(numsA).min().getAsInt();
-          int B = Arrays.stream(numsB).max().getAsInt();
-          int BB = Arrays.stream(numsB).min().getAsInt();
-          int[] numsElement = new int[4];
-          numsElement[0] = A;
-          numsElement[1] = AA;
-          numsElement[2] = B;
-          numsElement[3] = BB;
-          ArrayList<Integer> result = new ArrayList<>();
 
-          for (int i = 0; i <= 1; i++) {
-            for (int j = 2; j <= 3; j++) {
-              result.add(numsElement[i] * numsElement[j]);
-            }
+          for (int i = 0; i < nums[0]; i++) {
+            String line = sc.nextLine();
+            long result = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).filter(n -> n == nums[2]).count();
+            System.out.println(result);
           }
 
-          int output = result.stream().mapToInt(n -> n).max().getAsInt();
-          System.out.println(output);
+
         } catch (NumberFormatException e) {
           System.out.println("不正な入力があります: " + e.getMessage());
         }
