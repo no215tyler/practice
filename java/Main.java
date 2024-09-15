2fedabd83ce26ccd930f371186080bef0105e6e4
no215tyler <no.215.tyler@gmail.com>
2024-09-15 23:31:50 +0900

    GPT_Java問題集11: 複数の例外のキャッチ

--- java/Main.java
+++ java/Main.java
@@ -1,19 +1,25 @@
-// 問題 10: 基本的な例外処理
-// 					ユーザーに数値を入力してもらい、10をその数値で割った結果を表示するプログラムを作成してください。
-// 					ただし、0で割ろうとした場合は、エラーメッセージを表示してください。
+// 問題 11: 複数の例外のキャッチ
+// 					配列の要素を表示するプログラムを作成し、配列の範囲外にアクセスしようとした場合と、
+//          数値を0で割ろうとした場合の両方を例外としてキャッチして処理してください。
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("数値を入力してください");
+      System.out.println("半角スペース区切りで数値を入力してください");
+      int[] nums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+      System.out.println("配列要素に対して割る数値を入力してください");
       int N = sc.nextInt();
 
       try {
-        int result = 10 / N;
-        System.out.println(String.format("徐算結果: %s", result));
+        for (int i = 0; i < nums.length; i++) {
+          int result = nums[i] / N;
+          System.out.println(String.format("徐算結果: %s", result));
+        }
       } catch (ArithmeticException e) {
         System.out.println("エラー / 0で割ることはできません: " + e.getMessage());
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("エラー / 配列外のインデックスは参照できません" + e.getMessage());
       }
 
       sc.close();
