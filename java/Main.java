8f3eb55b5c458edc489fb1f481ab1ea2247a6101
no215tyler <no.215.tyler@gmail.com>
2024-09-16 10:03:10 +0900

    GPT_Java問題集14: HashSet の使用

--- java/Main.java
+++ java/Main.java
@@ -1,39 +1,40 @@
-// 問題 13: ArrayList の使用
-// ユーザーから名前を入力してもらい、それをArrayListに追加していきます。
-// その後、リストにある全ての名前を表示するプログラムを作成してください。
+// 問題 14: HashSet の使用
+// HashSetを使って、ユーザーが入力する整数を格納します。
+// 同じ整数が入力された場合は、格納されません。
+// すべての整数を表示するプログラムを作成してください。
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
       int prompt = 0;
-      List<String> users = new ArrayList<>();
+      Set<Integer> nums = new HashSet<>();
       Scanner sc = new Scanner(System.in);
       while (prompt != 3) {
-        System.out.println("プロンプトを入力してください\n【1】: ユーザー追加\n【2】: ユーザー表示\n【3】: プログラム終了");
+        System.out.println("プロンプトを入力してください\n【1】: 追加\n【2】: 表示\n【3】: プログラム終了");
         prompt = Integer.parseInt(sc.nextLine());
         switch (prompt) {
           case 1:
-            addUser(users);
+            addNums(nums);
             break;
           case 2:
-            putsDisplay(users);
+            putsDisplay(nums);
             break;
         }
       }
       sc.close();
     }
 
-    public static List<String> addUser(List<String> list) {
-      System.out.println("ユーザー名を入力してください");
+    public static Set<Integer> addNums(Set<Integer> list) {
+      System.out.println("数値を入力してください");
       Scanner sc = new Scanner(System.in);
-      list.add(sc.nextLine());
+      list.add(sc.nextInt());
+      // 配列を昇順ソート
+      list.stream().sorted();
       return list;
     }
 
-    public static void putsDisplay(List<String> list) {
-      for (int i = 1; i <= list.size(); i++) {
-        System.out.println(String.format("userid %d: %s", i, list.get(i - 1)));
-      }
+    public static void putsDisplay(Set<Integer> list) {
+      list.stream().forEach(System.out::println);
     }
 }
 
