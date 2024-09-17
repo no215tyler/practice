40aba0be888b0b928b7313e50e7f71d6e7b2a8e5
no215tyler <no.215.tyler@gmail.com>
2024-09-17 13:55:43 +0900

    GPT_Java問題集16: 配列の範囲外アクセス

--- java/Main.java
+++ java/Main.java
@@ -1,42 +1,15 @@
-// 問題 15: HashMap の使用
-// ユーザーに国名とその首都名を入力してもらい、それをHashMapに保存します。
-// すべての国と首都名のペアを表示するプログラムを作成してください。
+// 問題 16: 配列の範囲外アクセス
+// 次のプログラムでは、配列に対して範囲外のインデックスにアクセスしようとしています。
+// 例外処理を使って、エラーをキャッチし、適切なメッセージを表示するように修正してください。
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-      int prompt = 0;
-      Map<String, String> lists = new HashMap<>();
-      Scanner sc = new Scanner(System.in);
-      while (prompt != 3) {
-        System.out.println("プロンプトを入力してください\n【1】: 国名&首都を追加\n【2】: 一覧表示\n【3】: プログラム終了");
-        prompt = Integer.parseInt(sc.nextLine());
-        switch (prompt) {
-          case 1:
-            addCountry(lists);
-            break;
-          case 2:
-            putsDisplay(lists);
-            break;
-        }
+  public static void main(String[] args) {
+      int[] numbers = {1, 2, 3};
+      try {
+        System.out.println(numbers[3]); // ここで例外が発生
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("【エラー】範囲外にアクセスしようとしています: " + e.getMessage());
       }
-      sc.close();
-    }
-
-    public static Map<String, String> addCountry(Map<String, String> list) {
-      Scanner sc = new Scanner(System.in);
-      System.out.println("国名を入力してください");
-      String country = sc.nextLine();
-      System.out.println(String.format("%sの首都を入力してください", country));
-      String capitalCities = sc.nextLine();
-      list.put(country, capitalCities);
-      return list;
-    }
-
-    public static void putsDisplay(Map<String, String> list) {
-      for (Map.Entry<String, String> entry : list.entrySet()) {
-        System.out.println(String.format("%sの首都: %s", entry.getKey(), entry.getValue()));
-      }
-    }
+  }
 }
-
