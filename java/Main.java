7b23f3b880c33de4ff84addc02dd3b38718478f3
no215tyler <no.215.tyler@gmail.com>
2024-09-19 01:04:42 +0900

    残り物の量 Java編

--- java/Main.java
+++ java/Main.java
@@ -1,33 +1,18 @@
-// 検索ワード W が以前に入力されたことがある場合：
-// 履歴中の W を削除する。
-// 履歴の先頭に W を追加する。
-// 検索ワード W が以前に入力されたことがない場合：
-// 履歴の先頭に W を追加する。
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      List<String> list = new ArrayList<>();
+      List<Double> mpq = Arrays.stream(sc.nextLine().split(" "))
+                        .map(Double::parseDouble)
+                        .collect(Collectors.toList());
 
-      int N = Integer.parseInt(sc.nextLine());
 
-      for (int i = 0; i < N; i++) {
-        String input = sc.nextLine();
-        // inputがlist内にあるかチェック
-        int index = list.indexOf(input);
-        if (index == -1) {
-          // list内になければ先頭に追加
-          list.add(0, input);
-        } else {
-          // 【1】list内にあれば要素を削除
-          list.remove(index);
-          // 【2】先頭に要素を追加
-          list.add(0, input);
-        }
-      }
-      list.stream().forEach(System.out::println);
+      Double result = (mpq.get(0) * (1 - mpq.get(1) / 100)) * (1 - mpq.get(2) / 100);
+
+      System.out.println(result);
 
       sc.close();
     }
