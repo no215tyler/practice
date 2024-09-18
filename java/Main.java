383eb0456a13fd299727a64aadb53c969015ca1f
no215tyler <no.215.tyler@gmail.com>
2024-09-18 15:33:10 +0900

    Javaのカスタムパッケージ管理

--- java/Main.java
+++ java/Main.java
@@ -1,26 +1,8 @@
-// 問題 17: ファイル読み込みの例外処理
-// ユーザーからファイル名を入力してもらい、そのファイルを読み込むプログラムを作成してください。
-// ファイルが存在しない場合、`FileNotFoundException`を処理して適切なエラーメッセージを表示するようにしてください。
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import samplepack.Sample;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.print("読み込むファイル名を入力してください: ");
-        String filename = inputScanner.nextLine();
-
-        try {
-            File file = new File(filename);
-            Scanner fileScanner = new Scanner(file);
-
-            while (fileScanner.hasNextLine()) {
-                System.out.println(fileScanner.nextLine());
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("ファイルが見つかりません: " + e.getMessage());
-        }
+      Sample.sample();
     }
 }
---
+++ samplepack/Sample.java
@@ -0,0 +1,7 @@
+package samplepack;
+
+public class Sample {
+  public static void sample() {
+    System.out.println("sample.java");
+  }
+}
