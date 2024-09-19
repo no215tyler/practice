dd0fd30cdb7436401b556d93f96f7775d8f72171
no215tyler <no.215.tyler@gmail.com>
2024-09-19 15:31:54 +0900

    単語のカウント Java編

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-      ElementCount.elementCount();
+      Scanner sc = new Scanner(System.in);
+      // 入力を半角スペースで配列化
+      String[] strs = sc.nextLine().split(" ");
+
+      // 要素の重複を除いて配列化
+      List<String> uniqueStrs = new ArrayList<>();
+      for (String str : strs) {
+        long index = uniqueStrs.stream().filter(s -> s.equals(str)).count();
+        if (index == 0) {
+          uniqueStrs.add(str);
+        }
+      }
+
+      // 要素のカウント
+      for (String str : uniqueStrs) {
+        long cnt = Arrays.stream(strs).filter(s -> s.equals(str)).count();
+        System.out.println(String.format("%s %d", str, cnt));
+      }
+      sc.close();
     }
 }
