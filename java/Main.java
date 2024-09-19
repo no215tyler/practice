27397fd72e02b4a93f4500bf5188486afdf61e0d
no215tyler <no.215.tyler@gmail.com>
2024-09-19 17:22:27 +0900

    宝くじ Java編

--- java/Main.java
+++ java/Main.java
@@ -1,25 +1,66 @@
+// 【宝くじ】
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      // 入力を半角スペースで配列化
-      String[] strs = sc.nextLine().split(" ");
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // 1等の当選番号
+    String[] arrayWinnerNum = sc.nextLine().split("");
+    String winnerNum = Arrays.stream(arrayWinnerNum).map(String::valueOf).collect(Collectors.joining(""));
 
-      // 要素の重複を除いて配列化
-      List<String> uniqueStrs = new ArrayList<>();
-      for (String str : strs) {
-        long index = uniqueStrs.stream().filter(s -> s.equals(str)).count();
-        if (index == 0) {
-          uniqueStrs.add(str);
-        }
-      }
-
-      // 要素のカウント
-      for (String str : uniqueStrs) {
-        long cnt = Arrays.stream(strs).filter(s -> s.equals(str)).count();
-        System.out.println(String.format("%s %d", str, cnt));
-      }
-      sc.close();
+    // 2等の当選番号
+    List<String> lastFourDigits = new ArrayList<>();
+    for (int i = 2; i < 6 ; i++) {
+      lastFourDigits.add(arrayWinnerNum[i]);
     }
+    String secondWinnerNum = lastFourDigits.stream().collect(Collectors.joining(""));
+
+    // 3等の当選番号
+    List<String> lastThreeDigits = new ArrayList<>();
+    for (int i = 3; i < 6 ; i++) {
+      lastThreeDigits.add(arrayWinnerNum[i]);
+    }
+    String threeWinnerNum = lastThreeDigits.stream().collect(Collectors.joining(""));
+
+    // 購入数
+    int N = Integer.parseInt(sc.nextLine());
+
+    // メイン処理
+    for (int i = 0; i < N; i++) {
+      String input = sc.nextLine();
+      String[] ticketNums = input.split("");
+
+      // 2等のスコープ
+      List<String> ticketSecondNum = new ArrayList<>();
+      for (int j = 2; j < 6; j++) {
+        ticketSecondNum.add(ticketNums[j]);
+      }
+
+      // 3等のスコープ
+      List<String> ticketThirdNum = new ArrayList<>();
+      for (int j = 3; j < 6; j++) {
+        ticketThirdNum.add(ticketNums[j]);
+      }
+
+      // 1等の場合: first : 当選番号と一致する番号
+      if (input.equals(winnerNum)) {
+        System.out.println("first");
+      } else if (Integer.parseInt(winnerNum) == 100000 && Integer.parseInt(input) == 100001) { // 前後賞の場合: adjacent : 当選番号の ±1 の番号（当選番号が 100000 または 199999 の場合，前後賞は一つのみ）
+        System.out.println("adjacent");
+      } else if (Integer.parseInt(winnerNum) == 199999 && Integer.parseInt(input) == 199998) { // 同上
+        System.out.println("adjacent");
+      } else if (Integer.parseInt(winnerNum) + 1 == Integer.parseInt(input) || Integer.parseInt(winnerNum) - 1 == Integer.parseInt(input)) {
+        System.out.println("adjacent");
+      } else if (secondWinnerNum.equals(ticketSecondNum.stream().collect(Collectors.joining("")))) { // 2等の場合: second : 当選番号と下 4 桁が一致する番号（1等に該当する番号を除く）
+        System.out.println("second");
+      } else if (threeWinnerNum.equals(ticketThirdNum.stream().collect(Collectors.joining("")))) { // 3等の場合: third : 当選番号と下 3 桁が一致する番号（1等および2等に該当する番号を除く）
+        System.out.println("third");
+      } else { // それ以外（外れ）の場合: blank
+        System.out.println("blank");
+      }
+    }
+
+    sc.close();
+  }
 }
