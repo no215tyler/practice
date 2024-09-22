ab5c9e3c0537b818ce67af9b1a37ab09a1a3fe0b
no215tyler <no.215.tyler@gmail.com>
2024-09-22 10:48:33 +0900

    先頭の要素の削除(query) Java編

--- java/Main.java
+++ java/Main.java
@@ -3,24 +3,31 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int[] input = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    int N = input[0];
+    int K = input[1];
 
-    int[] NKQ = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    int N = NKQ[0]; // 要素数 = ループ回数
-    int index = NKQ[1]; // 配列添字
-    int Q = NKQ[2]; // 挿入する数値
-
-    // 動的配列
     List<Integer> list = new ArrayList<>();
+
     for (int i = 0; i < N; i++) {
       list.add(Integer.parseInt(sc.nextLine()));
     }
 
-    // 指定のインデックスへQを挿入
-    list.add(index, Q);
+    for (int i = 0; i < K; i++) {
+      String prompt = sc.nextLine();
+      if (prompt.equals("pop")) {
+        pop(list);
+      } else if (prompt.equals("show")) {
+        show(list);
+      }
+    }
+  }
 
-    // 出力
+  static void pop(List<Integer> list) {
+    list.remove(0);
+  }
+
+  static void show(List<Integer> list) {
     list.stream().forEach(System.out::println);
-
-    sc.close();
   }
 }
