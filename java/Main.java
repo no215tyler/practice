46972d57e7032b4d6b05af4c616726c1323dd95d
no215tyler <no.215.tyler@gmail.com>
2024-09-24 07:23:31 +0900

    2項間漸化式 1 Java編

--- java/Main.java
+++ java/Main.java
@@ -4,59 +4,15 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    // 初期入力
-    int[] nums = Arrays.stream(sc.nextLine()
-                      .split(" ")).mapToInt(Integer::parseInt)
-                      .toArray();
-    int N = nums[0];
-    int K = nums[1];
+    int[] XDK = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    List<Integer> list = new ArrayList<>(Arrays.asList(XDK[0]));
 
-    List<String> members = new ArrayList<>();
-
-    // 初期メンバー
-    for (int i = 0; i < N; i++) {
-      members.add(sc.nextLine());
+    for (int i = 1; i < XDK[2]; i++) {
+      list.add(list.get(i - 1) + XDK[1]);
     }
 
-    // イベント分岐
-    for (int i = 0; i < K; i++) {
-      String[] query = sc.nextLine().split(" ");
-      String prompt = query[0];
-      // 握手会
-      if (prompt.equals("handshake")) {
-        handShake(members);
-      // メンバー加入
-      } else if (prompt.equals("join")) {
-        String member = query[1];
-        join(members, member);
-      // メンバー脱退
-      } else if (prompt.equals("leave")) {
-        String member = query[1];
-        leave(members, member);
-      } else {
-        System.out.println("error: 予期せぬエラー");
-      }
-    }
+    System.out.println(list.get(XDK[2] - 1));
 
     sc.close();
-  }
-
-  static void join(List<String> members, String name) {
-    members.add(name);
-  }
-
-  static void leave(List<String> members, String name) {
-    int index = members.indexOf(name);
-    // 名前が存在しない場合のエラー対策
-    if (index >= 0) {
-      members.remove(index);
-    }
-  }
-
-  static void handShake(List<String> members) {
-    if (members.size() > 0) {
-      Collections.sort(members);
-      members.stream().forEach(System.out::println);
-    }
   }
 }
