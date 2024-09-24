2166ef41f1d3c74d4eed64dce60944ed18bf149d
no215tyler <no.215.tyler@gmail.com>
2024-09-24 22:59:07 +0900

    文字列の重複カウント Java編

--- java/Main.java
+++ java/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
   public static void main(String[] args) {
@@ -6,10 +7,39 @@ public class Main {
 
     String trgt = sc.nextLine();
     String[] strs = sc.nextLine().split("");
+    List<String> list = new ArrayList<>();
 
-    long count = Arrays.stream(strs).filter(s -> s.equals(trgt)).count();
+    for (int i = 1; i < strs.length; i++) {
+      List<String> bufStr = new ArrayList<>();
+      for (int j = trgt.length() - 1; j >= 0; j--) {
+        bufStr.add(strs[i - j]);
+      }
+      list.add(bufStr.stream().collect(Collectors.joining("")));
+    }
+
+    long count = list.stream().filter(s -> s.equals(trgt)).count();
     System.out.println(count);
 
     sc.close();
   }
 }
+
+// 【模範解答】
+// import java.util.Scanner;
+
+// public class Main {
+//   public static void main(String[] args) {
+//     Scanner scan = new Scanner(System.in);
+//     String pattern = scan.nextLine();
+//     String str = scan.nextLine();
+
+//     int cnt = 0;
+//     for (int i = 0; i <= str.length() - pattern.length(); i++) {
+//         String s = str.substring(i, i + pattern.length());
+//         if (s.equals(pattern)) {
+//           cnt++;
+//         }
+//     }
+//     System.out.println(cnt);
+//   }
+// }
