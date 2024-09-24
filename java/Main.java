04bfb11a77ffd99d07683d6c8608fdae67eda6f2
no215tyler <no.215.tyler@gmail.com>
2024-09-24 16:33:54 +0900

    【マップの扱い 1】マップの書き換え・1 マス Java編

--- java/Main.java
+++ java/Main.java
@@ -4,15 +4,39 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    int[] XDK = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    List<Integer> list = new ArrayList<>(Arrays.asList(XDK[0]));
+    // 盤面マス目の定義
+    int[] nums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-    for (int i = 1; i < XDK[2]; i++) {
-      list.add(list.get(i - 1) + XDK[1]);
+    int H = nums[0];
+    int W = nums[1];
+
+    List<List<String>> boards = new ArrayList<>();
+
+    for (int i = 0; i < H; i++) {
+      List<String> line = new ArrayList<>(Arrays.stream(sc.nextLine().split("")).toList());
+      boards.add(line);
     }
 
-    System.out.println(list.get(XDK[2] - 1));
+    // 更新するマス目の定義
+    int[] indexNums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    int y = indexNums[0];
+    int x = indexNums[1];
+
+    changeCell(boards, y, x);
+
+    for (List<String> board : boards) {
+      System.out.println(String.join("", board));
+    }
 
     sc.close();
+  }
+
+  static void changeCell(List<List<String>> boards, int y, int x) {
+    String target = boards.get(y).get(x);
+    if (target.equals("#")) {
+      boards.get(y).set(x, ".");
+    } else {
+      boards.get(y).set(x, "#");
+    }
   }
 }
