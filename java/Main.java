0c3bd031512bf5a4a7dbe96e8ccc7d478612c5d9
no215tyler <no.215.tyler@gmail.com>
2024-09-24 17:46:21 +0900

    【マップの扱い 2】マップの書き換え・縦横 Java編

--- java/Main.java
+++ java/Main.java
@@ -22,7 +22,7 @@ public class Main {
     int y = indexNums[0];
     int x = indexNums[1];
 
-    changeCell(boards, y, x);
+    checkChangeCell(boards, y, x);
 
     for (List<String> board : boards) {
       System.out.println(String.join("", board));
@@ -31,8 +31,33 @@ public class Main {
     sc.close();
   }
 
-  static void changeCell(List<List<String>> boards, int y, int x) {
+  static void checkChangeCell(List<List<String>> boards, int y, int x) {
     String target = boards.get(y).get(x);
+    changeCell(boards, y, x, target);
+
+    // 上の処理 yが0以外なら上を変更
+    if (y != 0) {
+      String subTarget = boards.get(y - 1).get(x);
+      changeCell(boards, y - 1, x, subTarget);
+    }
+    // 下の処理 yがy.size未満なら下を変更
+    if (y != boards.size() - 1) {
+      String subTarget = boards.get(y + 1).get(x);
+      changeCell(boards, y + 1, x, subTarget);
+    }
+    // 左の処理 xが0以外なら左を変更
+    if (x != 0) {
+      String subTarget = boards.get(y).get(x - 1);
+      changeCell(boards, y, x - 1, subTarget);
+    }
+    // 右の処理 xがx.size未満なら右を変更
+    if (x != boards.get(y).size() - 1) {
+      String subTarget = boards.get(y).get(x + 1);
+      changeCell(boards, y, x + 1, subTarget);
+    }
+  }
+
+  static void changeCell(List<List<String>> boards, int y, int x, String target) {
     if (target.equals("#")) {
       boards.get(y).set(x, ".");
     } else {
