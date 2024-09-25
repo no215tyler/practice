fb6822633e5e1b2f9a5f0a9b1318c9ad7c2380bb
no215tyler <no.215.tyler@gmail.com>
2024-09-25 15:32:14 +0900

    GPT_Java問題集21: ラムダ式

--- java/Main.java
+++ java/Main.java
@@ -1,41 +1,15 @@
-// 問題 20: Mapの使用
-// フルーツとその価格を格納する`HashMap`を作成し、いくつかのキーと値のペアを追加してください。
-// その後、特定のフルーツの価格を取得して出力し、すべてのキーと値のペアをループで出力するプログラムを作成してください。
+// 問題 21: ラムダ式
+// 整数のリストを作成し、その中から3の倍数の要素だけをフィルタリングして、各要素を3倍にして出力してください。
 import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    Map<String, Integer> fruitsStore = new HashMap<>();
-
-    int query = 1;
-    while (query != 0) {
-      try {
-        System.out.println("【果物名】と【価格】を半角スペース区切りで入力してください");
-        String[] input = sc.nextLine().split(" ");
-        fruitsStore.put(input[0], Integer.parseInt(input[1]));
-        System.out.println("終了する場合は【0】を入力してください");
-        query = Integer.parseInt(sc.nextLine());
-      } catch (NumberFormatException e) {
-        // エラーを無視
-      }
-    }
-
-    // HashMapをリストへ変換し
-    List<Map.Entry<String, Integer>> list = new ArrayList<>(fruitsStore.entrySet());
-    // 価格順に昇順ソート
-    Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
-      public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-        return o1.getValue().compareTo(o2.getValue());
-      }
+    List<Integer> nums = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+    nums.stream().filter(n -> n % 3 == 0).forEach(n -> {
+      System.out.println(n * 3);
     });
-
-    // 出力処理
-    for (Map.Entry<String, Integer> entry : list) {
-      System.out.println(String.format("%s : %d", entry.getKey(), entry.getValue()));
-    }
-
 
     sc.close();
   }
