51c75f257a7541c64dc8787f103ef2ad3c212a87
no215tyler <no.215.tyler@gmail.com>
2024-09-25 16:12:30 +0900

    GPT_Java問題集23: Optional

--- java/Main.java
+++ java/Main.java
@@ -1,16 +1,41 @@
-// 問題 22: Stream API
-// 文字列のリストを作成し、名前が5文字以上のものをすべて小文字に変換し、その結果をリストとして収集してください。
+// 問題 23: Optional
+// `getAge()`メソッドを実装し、年齢を取得します。
+// この年齢が`null`でない場合は年齢を表示し、`null`の場合は「年齢が未設定です」と表示するプログラムを作成してください。
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    List<String> list = new ArrayList<>(Arrays.asList("BLACK", "BLUE", "RED", "WHITE", "YELLOW", "BROWN"));
+    List<Person> persons = new ArrayList<>();
 
-    List<String> result = list.stream().filter(s -> s.length() >= 5).map(String::toLowerCase).collect(Collectors.toList());
-    result.stream().forEach(System.out::println);
+    for (int i = 0; i < 3; i++) {
+      System.out.println("名前と年齢を半角スペース区切りで入力してください");
+      String[] input = sc.nextLine().split(" ");
+      if (input.length == 2) {
+        persons.add(new Person(input[0], Optional.of(Integer.parseInt(input[1]))));
+      } else {
+        persons.add(new Person(input[0]));
+      }
+      if (persons.get(i).age == null) {
+        System.out.println("年齢が未設定です");
+      }
+    }
     sc.close();
+  }
+}
+
+class Person {
+  String name;
+  Optional<Integer> age = null;
+
+  public Person(String name, Optional<Integer> age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public Person(String name) {
+    this.name = name;
   }
 }
