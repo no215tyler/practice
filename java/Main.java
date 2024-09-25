437b26dd002fcf49341b419605593652294ed4ec
no215tyler <no.215.tyler@gmail.com>
2024-09-25 21:25:59 +0900

    文字と整数の組のソート2 Java編

--- java/Main.java
+++ java/Main.java
@@ -1,6 +1,3 @@
-// 問題 23: Optional
-// `getAge()`メソッドを実装し、年齢を取得します。
-// この年齢が`null`でない場合は年齢を表示し、`null`の場合は「年齢が未設定です」と表示するプログラムを作成してください。
 import java.util.*;
 
 
@@ -8,34 +5,30 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    List<Person> persons = new ArrayList<>();
+    int N = Integer.parseInt(sc.nextLine());
+    Map<String, Integer> hash = new HashMap<>();
 
-    for (int i = 0; i < 3; i++) {
-      System.out.println("名前と年齢を半角スペース区切りで入力してください");
+    for (int i = 0; i < N; i++) {
       String[] input = sc.nextLine().split(" ");
-      if (input.length == 2) {
-        persons.add(new Person(input[0], Optional.of(Integer.parseInt(input[1]))));
-      } else {
-        persons.add(new Person(input[0]));
-      }
-      if (persons.get(i).age == null) {
-        System.out.println("年齢が未設定です");
-      }
+      hash.merge(input[0], Integer.parseInt(input[1]), Integer::sum);
     }
+
+    // HashMapをリストに変換
+    List<Map.Entry<String, Integer>> list = new ArrayList<>(hash.entrySet());
+
+    // 降順ソート
+    Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+      @Override
+      public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+        return o2.getValue().compareTo(o1.getValue());
+      }
+    });
+
+    // 出力処理
+    for (Map.Entry<String, Integer> entry : list) {
+      System.out.println(String.format("%s %d", entry.getKey(), entry.getValue()));
+    }
+
     sc.close();
-  }
-}
-
-class Person {
-  String name;
-  Optional<Integer> age = null;
-
-  public Person(String name, Optional<Integer> age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  public Person(String name) {
-    this.name = name;
   }
 }
