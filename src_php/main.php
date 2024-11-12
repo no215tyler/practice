b7d6d2668cca06d12f1636134f624ede33d544c8
no215tyler <no.215.tyler@gmail.com>
2024-11-12 22:58:52 +0900

    php: 二次元配列の入出力

--- src_php/main.php
+++ src_php/main.php
@@ -1,7 +1,14 @@
 <?php
-  $S = gmp_init(trim(fgets(STDIN)));
-  $T = gmp_init(trim(fgets(STDIN)));
-  $result = gmp_mul($S, $T);
-  echo $result;
-  echo "\n";
+  $input = explode(" ", trim(fgets(STDIN)));
+  $N = $input[0];
+  $r = $input[2] - 1;
+  $c = $input[3] - 1;
+
+  $array = [];
+  for($i = 0; $i < $N; $i++) {
+    $row = explode(" ", trim(fgets(STDIN)));
+    $array[] = $row;
+  }
+
+  echo $array[$r][$c] . "\n";
 ?>
