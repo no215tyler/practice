7bd5b251d55cd558041b00254b6760b389cb697f
no215tyler <no.215.tyler@gmail.com>
2024-11-12 22:55:50 +0900

    php: 大きな数の掛け算（bcmul）

--- src_php/main.php
+++ src_php/main.php
@@ -1,13 +1,7 @@
 <?php
-  $N = trim(fgets(STDIN));
-  $nums = explode(" ", trim(fgets(STDIN)));
-  $count = array_count_values($nums);
-  for ($i = 0; $i <= 9; $i++) {
-    echo isset($count[$i]) ? $count[$i] : 0;
-    if ($i < 9) {
-      echo " ";
-    } else {
-      echo "\n";
-    }
-  }
+  $S = strval(trim(fgets(STDIN)));
+  $T = strval(trim(fgets(STDIN)));
+  $result = bcmul($S, $T);
+  echo $result;
+  echo "\n";
 ?>
